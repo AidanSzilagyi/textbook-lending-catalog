64a8877149b87a54c3aeca70621188bff574e58c
devpp101 <devpp101@gmail.com>
2025-02-20 20:53:51 -0500

    added a model

--- mainmenu/models.py
+++ mainmenu/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class TestObject(models.Model):
     important_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
+class Account(models.Model):
+    user = models.CharField("user", max_length=200)
+    email = models.CharField("email", max_length=200)
+    password = models.CharField("password", max_length=200)
+    
