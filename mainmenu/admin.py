98d04a60559aed3a4713bc9700690b6f05011060
devpp101 <devpp101@gmail.com>
2025-02-21 15:13:53 -0500

    changed the admin.py to make the profile database include username email role first_name, last_name, and password. Password looks hashed on the database. We can also change the userRole to either make them a librarian or patreon on the admin page.

--- mainmenu/admin.py
+++ mainmenu/admin.py
@@ -5,5 +5,33 @@ from django.contrib import admin
 from .models import TestObject,Profile
 
 admin.site.register(TestObject)
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    # Define which fields to display in the admin list view
+    list_display = ('user', 'user_username', 'user_email', 'userRole', 'user_first_name', 'user_last_name', 'user_password')
+
+    # Custom method to display the username of the associated User model
+    def user_username(self, obj):
+        return obj.user.username
+    user_username.short_description = 'Username'  # Optional: Set a custom header for this column
+
+    # Custom method to display the email of the associated User model
+    def user_email(self, obj):
+        return obj.user.email
+    user_email.short_description = 'Email'
+
+    # You can add more custom methods to show other User fields if needed
+    def user_first_name(self, obj):
+        return obj.user.first_name
+    user_first_name.short_description = 'First Name'
+
+    def user_last_name(self, obj):
+        return obj.user.last_name
+    user_last_name.short_description = 'Last Name'
+
+    def user_password(self, obj):
+        return obj.user.password
+    user_password.short_description = 'Password'
+
+admin.site.register(Profile, ProfileAdmin)
 
