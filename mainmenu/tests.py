a8161ee62b8cbed11af21ac3f2ad72b6432adbbf
Surya Vemulapalli <iciclecrash50@gmail.com>
2025-03-15 18:17:44 -0400

    Added imports

--- mainmenu/tests.py
+++ mainmenu/tests.py
@@ -2,16 +2,21 @@ from django.test import TestCase, Client
 from .models import Profile, create_user_profile
 from django.urls import reverse
 import requests
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, UserManager
+
 
 
 
 
 # Create your tests here.
+
+
+
 class LibProfileViewTests(TestCase):
     def setUp(self):
         self.client = Client()
-        self.Profile = create_profile(user=create_user(username="user", password="passwerd"), userRole=1)
+        self.user = User.objects.create_user(username='user', password='passwerd')
+        self.profile = create_user_profile(user=self.user, userRole=1)
         self.url = reverse('librarian_homepage')
 
     def test_librarian_logged_in(self):
