abcf4624517f09b425307f7740163d39284b7185
Surya Vemulapalli <iciclecrash50@gmail.com>
2025-04-29 11:13:37 -0400

    Fixed some issues with the test cases

--- mainmenu/tests.py
+++ mainmenu/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, Client
-from .models import Profile
+from .models import Profile, Item, Message
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
